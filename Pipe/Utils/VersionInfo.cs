ecc9cb961a1fd7eb78913aeca00907ff52791111
Konstantin Zhigaylo <kostyantop8@gmail.com>
2023-01-20 16:40:50 +0300

    Change version and RC number

--- Pipe/Utils/VersionInfo.cs
+++ Pipe/Utils/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace Pipe.Utils;
 
 public static class VersionInfo
 {
-   public static readonly string Version = "2.1";
-   public static readonly string ReleaseCandidate = "final";
+   public static readonly string Version = "2.2";
+   public static readonly string ReleaseCandidate = "1";
 }

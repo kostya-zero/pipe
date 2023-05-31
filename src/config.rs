ede796159763364d42122b65a3e4ce7cc8a20733
Konstantin Zhigaylo <kostyantop8@gmail.com>
2023-05-31 17:41:52 +0300

    config: implement default for project structure and made config load.

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,43 @@
+use std::fs;
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Info {
     pub name: String,
     pub version: String,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Module {
-    pub name: String,
-    pub version: String,
+#[derive(Deserialize, Serialize)]
+pub struct Depends {
+    pub modules: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Project {
     pub info: Info,
-    pub modules: Vec<Module>,
+    pub depends: Depends,
+}
+
+impl Default for Project {
+    fn default() -> Self {
+        Project {
+            info: Info {
+                name: String::from("PipeProject"),
+                version: String::from("0.1.0"),
+            },
+            depends: Depends { modules: vec![] },
+        }
+    }
+}
+
+pub struct Manager;
+impl Manager {
+    pub fn load_project() -> Project {
+        let content =
+            fs::read_to_string("pipe.yml").expect("Failed to read file or file not found.");
+        let config: Project =
+            serde_yaml::from_str(&content).expect("Failed to fetch config. Maybe syntax issue.");
+        config
+    }
 }

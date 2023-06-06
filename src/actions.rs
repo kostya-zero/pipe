4b1b855848446f412aea98ee3eb9012307c75c63
Konstantin Zhigaylo <kostyantop8@gmail.com>
2023-06-06 21:43:50 +0300

    cargo: `fmt` and `update`

--- src/actions.rs
+++ src/actions.rs
@@ -1,13 +1,21 @@
-use crate::{term::Term, config::{Project, Manager}};
+use crate::{
+    config::{Manager, Project},
+    term::Term,
+};
 
 pub struct Actions;
 impl Actions {
     pub fn init() {
         let name: String = Term::ask("How will be named your project?", "PipeProject".to_string());
-        let version: String = Term::ask("Which version of your project? It will be used for executable metadata.", "0.1.0".to_string());
-        
+        let version: String = Term::ask(
+            "Which version of your project? It will be used for executable metadata.",
+            "0.1.0".to_string(),
+        );
+
         Term::msg("Generating project configuration...");
-        let mut base_config: Project = Project { ..Default::default() };
+        let mut base_config: Project = Project {
+            ..Default::default()
+        };
         base_config.info.name = name;
         base_config.info.version = version;
         Manager::write(base_config);

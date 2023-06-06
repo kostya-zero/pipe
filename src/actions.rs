903c5ac56eafcbb4fc31ee3b52e20a718e500203
Konstantin Zhigaylo <kostyantop8@gmail.com>
2023-06-06 21:43:07 +0300

    actions: implement initialization of project configuration.

--- src/actions.rs
+++ src/actions.rs
@@ -1,4 +1,16 @@
+use crate::{term::Term, config::{Project, Manager}};
+
 pub struct Actions;
 impl Actions {
-    pub fn init() {}
+    pub fn init() {
+        let name: String = Term::ask("How will be named your project?", "PipeProject".to_string());
+        let version: String = Term::ask("Which version of your project? It will be used for executable metadata.", "0.1.0".to_string());
+        
+        Term::msg("Generating project configuration...");
+        let mut base_config: Project = Project { ..Default::default() };
+        base_config.info.name = name;
+        base_config.info.version = version;
+        Manager::write(base_config);
+        Term::msg("Project file has been created! It is called \"pipe.yml\".");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use actions::Actions;
+
 use crate::args::get_args;
 
 mod actions;
@@ -8,7 +10,9 @@ mod term;
 fn main() {
     let args = get_args().get_matches();
     match args.subcommand() {
-        Some(("init", _sub)) => {}
+        Some(("init", _sub)) => {
+            Actions::init();
+        }
         _ => {
             println!("Unknown command.")
         }

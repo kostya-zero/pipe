1038a1292e1cab12d82c546f8650cbfe317e01a6
Konstantin Zhigaylo <zero@kostyazero.com>
2024-02-16 22:08:21 +0300

    chore: new project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::process::exit;
+
+use actions::Actions;
+use term::Term;
+
+use crate::args::get_args;
+
+mod actions;
+mod args;
+mod config;
+mod term;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_args().get_matches();
+    match args.subcommand() {
+        Some(("init", _sub)) => {
+            Actions::init();
+        }
+        Some(("add", _sub)) => {
+            let package: String = _sub.get_one::<String>("package").expect("Failed to get argument.").to_string();
+
+            if package.is_empty() {
+                Term::msg("Cannot add an \"empty\" package.");
+                exit(1);
+            }
+
+            Actions::add(&package);
+        }
+        _ => {
+            println!("Unknown command.")
+        }
+    }
 }
